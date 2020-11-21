5f819d95cd021027b10d7e5efbff2f4e09b6db7f
kminami0 <44050041+kminami0@users.noreply.github.com>
2020-11-21 17:12:45 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+$(function(){
+  $(.item).click(function(){
+    var $abstract = $(this).find('.abstract');
+    if($abstract.hasClass('open')){
+      $abstract.removeClass('open');
+      $abstract.slideUp();
+      $(this).find('span').text('+');
+    }
+    else{
+      $abstract.addClass('open');
+      $abstract.slideDown();
+      $(this).find('span').text('-');
+    }
+  });
+
+  $("div").css("background-color", "red");
+});
